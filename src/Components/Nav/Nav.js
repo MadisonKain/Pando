59578307de77786ea827b6838dc9e731a238f6cc
Madison <madisonlkain@gmail.com>
2018-04-04 10:42:24 -0600

    first commit

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    
+
+    render(){
+        return(
+            <div>
+                NAV
+            </div>
+        )
+    }
+}
