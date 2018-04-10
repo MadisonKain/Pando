3084141bfcace5bfe8df9bb475a36f48d9e99e3a
Madison <madisonlkain@gmail.com>
2018-04-10 17:26:56 -0600

    Cart Updated

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import CartItem from '../CartItem/CartItem';
+import './Cart.css';
 
-class Cart extends Component{
-    constructor(){
+
+import { IconButton } from 'material-ui';
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import { deepOrange400 } from 'material-ui/styles/colors';
+import { orange50 } from 'material-ui/styles/colors';
+
+
+
+class Cart extends Component {
+    constructor() {
         super()
-        this.state={
-
+        this.state = {
+            cartItems: []
         }
     }
 
+    componentDidMount() {
+        axios.get('/cart')
+            .then(response => {
+                this.setState({
+                    cartItems: response.data
+                })
+            })
+    }
 
-    render(){
-        return(
-            <div> 
-                CART!
+
+    render() {
+        const product = this.state.cartItems.map(item => {
+            return <CartItem key={item.id}
+                products={item} />
+        })
+        console.log(product)
+        return (
+            <div>
+                <div className='pageTitle'>
+                    <IconButton>
+                        <ArrowBack hoverColor={orange50} />
+                    </IconButton>
+                    MY CART
+                </div>
+                <div>
+                    {product}
+                </div>
             </div>
         )
     }
