1099acec299ae7d5744be883b8da9292ecfe08ca
Madison <madisonlkain@gmail.com>
2018-04-10 17:32:15 -0600

    More Stufff

--- src/Components/CartItem/CartItem.js
+++ src/Components/CartItem/CartItem.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
+import './CartItem.css';
+import axios from 'axios';
+
+import { IconButton } from 'material-ui';
+import Delete from 'material-ui/svg-icons/action/delete';
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import { deepOrange400 } from 'material-ui/styles/colors';
+import { orange50 } from 'material-ui/styles/colors';
 
 class CartItem extends Component {
-    constructor(){
-        super()
+    constructor( props ){
+        super( props )
         this.state = {
 
         }
     }
 
+    handleClick(){
+        axios.delete( `/cart/delete/${this.props.products.id}` )
+    }
+
     render(){
+        const { name, picture, price, quantity } = this.props.products;
         return(
-            <div>
-                CartItem
+            <div className="mainBox">
+                <h1 className='title'>
+                    { name }
+                </h1>
+                <div className="itemContainer">
+                    <img 
+                        className="itemPhoto"
+                        src={ picture }
+                    />
+                    <div className='stuff'>
+                        <div>
+                            ${ price }
+                        </div>
+                        <div>
+                            Qty: { quantity }
+                        </div>
+                        <IconButton>
+                            <Delete hoverColor={ deepOrange400 } 
+                                    onClick={ ()=> this.handleClick() }/>
+                        </IconButton>
+                    </div>
+                </div>
             </div>
         )
     }
